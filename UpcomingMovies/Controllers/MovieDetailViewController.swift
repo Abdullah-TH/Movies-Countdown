fbb5f3702a42501b9f06a739105a48b2669d900a
Abdullah Althobetey <abdullah.th@me.comm>
2017-12-16 05:38:39 +0300

    Implement moving the countdown timer to the top or bottom in the movie detial screen

--- UpcomingMovies/Controllers/MovieDetailViewController.swift
+++ UpcomingMovies/Controllers/MovieDetailViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
+enum CountdownPosition
+{
+    case top
+    case bottom
+}
+
 class MovieDetailViewController: UIViewController
 {
     // MARK: Outlets
     @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var countdownStackView: UIStackView!
     @IBOutlet weak var yearsLabel: UILabel!
     @IBOutlet weak var monthsLabel: UILabel!
     @IBOutlet weak var daysLabel: UILabel!
@@ -19,11 +26,14 @@ class MovieDetailViewController: UIViewController
     @IBOutlet weak var minutesLabel: UILabel!
     @IBOutlet weak var secondsLabel: UILabel!
     @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var toolbar: UIToolbar!
     
     // MARK: Properties
     
     var movie: Movie!
     var timer = Timer()
+    var countdownStackViewCurrentConstraints = [NSLayoutConstraint]()
+    var countdownPosition = CountdownPosition.bottom
     
     // MARK: ViewController Methods
     
@@ -37,6 +47,8 @@ class MovieDetailViewController: UIViewController
         }
         
         setupUI()
+        view.addSubview(countdownStackView)
+        setupCountdownStackViewConstraints()
     }
     
     // MARK: Helper Methods
@@ -46,6 +58,48 @@ class MovieDetailViewController: UIViewController
         navigationItem.title = movie.title
         releaseDateLabel.text = "\(movie.allreadyReleased ? "Released" : "Releasing") \(movie.prettyDateString)"
         setPosterImage()
+    }
+    
+    private func setupCountdownStackViewConstraints()
+    {
+        countdownStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.removeConstraints(countdownStackViewCurrentConstraints)
+        countdownStackViewCurrentConstraints.removeAll()
+        
+        let leadingConst = NSLayoutConstraint(item: countdownStackView,
+                                              attribute: .leading,
+                                              relatedBy: .equal,
+                                              toItem: view,
+                                              attribute: .leading,
+                                              multiplier: 1,
+                                              constant: 0)
+        
+        let trailingConst = NSLayoutConstraint(item: countdownStackView,
+                                               attribute: .trailing,
+                                               relatedBy: .equal,
+                                               toItem: view,
+                                               attribute: .trailing,
+                                               multiplier: 1,
+                                               constant: 0)
+        
+        let bottomConst = NSLayoutConstraint(item: countdownStackView,
+                                             attribute: countdownPosition == .bottom ? .bottom : .top,
+                                             relatedBy: .equal,
+                                             toItem: countdownPosition == .bottom ? toolbar : view.safeAreaLayoutGuide,
+                                             attribute: .top,
+                                             multiplier: 1,
+                                             constant: 0)
+        
+        leadingConst.isActive = true
+        trailingConst.isActive = true
+        bottomConst.isActive = true
+        
+        countdownStackViewCurrentConstraints.append(contentsOf: [leadingConst, trailingConst, bottomConst])
+        
+        // Rearrange the stackview
+        let secondViewInStack = countdownStackView.arrangedSubviews[1]
+        countdownStackView.insertArrangedSubview(secondViewInStack, at: 0)
     }
     
     private func updateCountdownComponents()
@@ -86,6 +140,14 @@ class MovieDetailViewController: UIViewController
     {
     }
     
+    @IBAction func toggleCountdownPosition(_ sender: UIBarButtonItem)
+    {
+        countdownPosition = countdownPosition == .bottom ? .top : .bottom
+        
+        UIView.animate(withDuration: 0.2) {
+            self.setupCountdownStackViewConstraints()
+        }
+    }
 }
 
 
