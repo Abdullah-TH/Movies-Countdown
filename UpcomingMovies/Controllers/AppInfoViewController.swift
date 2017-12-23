269058e323e847296a873a4a36ac7a846e9ca521
Abdullah Althobetey <abdullah.th@me.comm>
2017-12-23 17:38:20 +0300

    Implement the app info screen

---
+++ UpcomingMovies/Controllers/AppInfoViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AppInfoViewController.swift
+//  UpcomingMovies
+//
+//  Created by Abdullah Althobetey on 23/12/2017.
+//  Copyright © 2017 Abdullah Althobetey. All rights reserved.
+//
+
+import UIKit
+
+class AppInfoViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func goBack(_ sender: UIButton)
+    {
+        dismiss(animated: true, completion: nil)
+    }
+}
